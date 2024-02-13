336471552b4d8de918471070d1488c6169bec83c
Palash-Halder <palashhalder.ju@gmail.com>
2024-02-13 15:43:50 +0600

    19_5 Introduction To Objects Properties And Values primitive and non-primitve data types

---
+++ module-19/object.js
@@ -0,0 +1,34 @@
+// Primitive- basic/primary
+const age = 21;
+const school = 'rifle sqaure';
+const isPassed = true;
+
+let isDeveloper;
+
+const sujects = ['Bangla', 'English', 'Physics', 'Math']
+
+console.log(isDeveloper); // ex of undefined
+
+
+// Object--> property: value
+
+// Non-Primitive
+
+const bottle = {
+    brand: 'apple',
+    price: 45,
+    color: 'white',
+    isClean: false
+}
+
+
+const subject = {
+    name: 'Biology',
+    teacher: 'rasheda mam',
+    examDate: '22 Feb',
+    chapters: ['first', 'second', 'third'],
+    exams: {
+        name: 'Final Exam',
+        marks: 100
+    }
+}
