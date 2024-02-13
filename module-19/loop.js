b8c82f9b6db0911830e34425abfd23f6023f9092
Palash-Halder <palashhalder.ju@gmail.com>
2024-02-13 23:16:35 +0600

    19_8 Loop An Object And Ways To Declare An Object

--- module-19/loop.js
+++ module-19/loop.js
@@ -1,3 +1,5 @@
+// Object Literal
+
 const mobile = {
     brand: 'Samsung',
     price: 25000,
@@ -13,3 +15,21 @@ for(const prop in mobile){
     console.log(prop);
     console.log(mobile[prop]);
 }
+
+const keys = Object.keys(mobile);
+console.log(keys);
+
+for(const key of keys){
+    console.log(key, ':', mobile[key]);
+}
+
+
+// Declare Object ------------->
+
+const pen = { brand: 'HP', price: 10, colors: 'black'}
+const pencil = new Object();
+console.log(pencil);
+const rubber = Object.create({});
+console.log(rubber);
+
+//using new
