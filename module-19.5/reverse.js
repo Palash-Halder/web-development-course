f953c08195ab29c551db6c6c743d4a83e13b1940
Palash-Halder <palashhalder.ju@gmail.com>
2024-02-14 17:17:14 +0600

    safe commit

---
+++ module-19.5/reverse.js
@@ -0,0 +1,18 @@
+
+/** 
+-- Reversing an array:
+*/
+const numbers = [1, 2, 3, 4, 5, 6, 7]
+//console.log(numbers);
+//numbers.reverse();
+//console.log(numbers);
+
+const reversed = numbers.reverse();
+
+console.log(reversed);
+
+
+const rev_numbers = [];
+for (const iterator of object) {
+    
+}
