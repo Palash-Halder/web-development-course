61e30a0b2a79e13c1972fb1fa53b0dcde58f633d
Palash-Halder <palashhalder.ju@gmail.com>
2024-02-15 10:18:46 +0600

    19.5_3 Sort An Array Problems And Solution

--- module-19.5/sort.js
+++ module-19.5/sort.js
@@ -1,10 +1,27 @@
-const persons = ['rakib', 'nokib', 'sakib', 'akib', 'dakib'];
+const persons = ['rakib', 'nokib', 'sakib', 'akib', 'dakib', 'anis', 'Anis'];
 const sortedPersons = persons.sort();
 console.log(sortedPersons);
 
 // sort -------->
-const numbers = [4, 7, 2, 8, 3, 6];
+//const numbers = [4, 7, 2, 8, 3, 6];
 
 /*
 Ascendng ------> Samaller to Larger: [2, 3, 4, 6, 7, 8 ];
-Descending -----> 
+Descending -----> Larger to Smaller: [8, 7, 6, 4, 3, 2];
+
+*/
+
+const numbers = [4, 7, 12, 8, 43, 6, 1];
+//const numbers_asc = numbers.sort(); // not working
+
+const numbers_asc = [...numbers].sort(function (a, b) {return a - b });
+const numbers_dsc = [...numbers].sort(function (a, b) {return b - a });
+console.log(numbers_asc);
+console.log(numbers_dsc);
+
+
+
+/**
+ * ========== Learn ASCII Binary Character Table ==========
+ */
+
