6dd2cfc3f41e956dd491607a01eaf6020cc6e066
Palash-Halder <palashhalder.ju@gmail.com>
2024-02-15 10:04:48 +0600

    19.5_2 Reversing An Array With/Without Reverse Method complete

--- module-19.5/reverse.js
+++ module-19.5/reverse.js
@@ -7,12 +7,40 @@ const numbers = [1, 2, 3, 4, 5, 6, 7]
 //numbers.reverse();
 //console.log(numbers);
 
-const reversed = numbers.reverse();
+//const reversed = numbers.reverse();
 
-console.log(reversed);
+//console.log(reversed);
 
 
 const rev_numbers = [];
-for (const iterator of object) {
-    
+
+for (const num of numbers) {
+    console.log(num);
+    rev_numbers.unshift(num);
 }
+
+console.log(rev_numbers);
+
+
+
+
+const reversed_numbers = [];
+for (let i = 0; i < numbers.length; i++) {
+    const num = numbers[i];
+    reversed_numbers.unshift(num);
+}
+
+
+//reverse side
+
+const  rev_rev_numbers = [];
+
+for (let i = numbers.length - 1; i >= 0; i-- ){
+    const num = numbers[i];
+    console.log(num);
+    rev_rev_numbers.push(num);
+}
+
+console.log(rev_rev_numbers);
+
+
---
+++ module-19.5/sort.js
@@ -0,0 +1,10 @@
+const persons = ['rakib', 'nokib', 'sakib', 'akib', 'dakib'];
+const sortedPersons = persons.sort();
+console.log(sortedPersons);
+
+// sort -------->
+const numbers = [4, 7, 2, 8, 3, 6];
+
+/*
+Ascendng ------> Samaller to Larger: [2, 3, 4, 6, 7, 8 ];
+Descending -----> 
