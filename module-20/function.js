ae91b5ececb6de42564fe218dbbfe29890282b73
Palash-Halder <palashhalder.ju@gmail.com>
2024-02-15 14:05:58 +0600

    20-1 What Are Function And Function Syntax

---
+++ module-20/function.js
@@ -0,0 +1,26 @@
+
+const price = 45;
+
+
+// Structure of function
+/**
+ * function name(params) {
+}
+ */
+// ডিক্লেয়ার ফাংশন
+function fanOffKor() {
+    console.log('বসা থেকে উঠে দাঁড়া');
+    console.log('সুইচের কাছে যা');
+    console.log('ফ্যানের সুইচ বন্ধ কর');
+}
+// function call
+fanOffKor();
+
+// Function name(){}
+function brushTeeth() {
+    console.log('ব্র্যাশ নাও');
+    console.log('ব্র্যাশে পেস্ট লাগাও');
+    console.log('২ মিনিট দাঁত মাজোঁ');
+}
+
+brushTeeth();
