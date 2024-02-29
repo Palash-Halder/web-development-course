a35845c587b4449640eb33068e4dc0578bbf5f87
Palash-Halder <palashhalder.ju@gmail.com>
2024-02-29 16:13:26 +0600

    second problem solved

--- module-22/assignment_four.js
+++ module-22/assignment_four.js
@@ -4,6 +4,33 @@
 
 // Problem- 02
 
+function checkName(name) {
+
+    
+    name = name.toLowerCase();
+
+    const letters = ['a', 'y', 'i', 'e', 'o', 'u', 'w' ];
+
+    const lastCharacter = name.charAt(name.length - 1);
+
+    if (letters.includes(lastCharacter)) {
+
+        return 'Good Name';
+        
+    } else if(typeof name != 'String'){
+
+        return 'invalid';
+
+    }else{
+        return 'Bad Name'
+    }
+
+}
+
+console.log(checkName('Pala'));
+
+
+
 // Problem- 03
 
 // Problem- 04
